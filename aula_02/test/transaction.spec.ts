48659c4aa81aa45549498d25ba41964f898e84f8
Moisés Macedo <moises.castro@concentrix.com>
2024-08-30 16:51:07 -0300

    test: added list all

--- aula_02/test/transaction.spec.ts
+++ aula_02/test/transaction.spec.ts
@@ -1,4 +1,4 @@
-import { test, beforeAll, afterAll, describe } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect } from 'vitest'
 import supertest from 'supertest'
 import { app } from '../src/app'
 
@@ -6,9 +6,11 @@ describe('Transaction routes', () => {
   beforeAll(async () => {
     await app.ready()
   })
+
   afterAll(async () => {
     await app.close()
   })
+
   test('User can create a new transaction', async () => {
     await supertest(app.server)
       .post('/transactions')
@@ -18,5 +20,31 @@ describe('Transaction routes', () => {
         type: 'CREDIT_CARD',
       })
       .expect(201)
+  })
+
+  test('Should be able to list all transactions', async () => {
+    const transactionValue = {
+      title: `teste ${Math.random() * 10}`,
+      amout: 1000,
+    }
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({ ...transactionValue, type: 'CREDIT_CARD' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies as string[])
+      .expect(200)
+      .then((res) => res.body)
+
+    expect(listTransactionsResponse).toEqual([
+      expect.objectContaining({
+        ...transactionValue,
+      }),
+    ])
+
+    console.log(listTransactionsResponse)
   })
 })
