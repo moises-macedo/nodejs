4d6f561f1d8dbc6935cac7e520d4e67723559e64
Moisés Macedo <moises.castro@concentrix.com>
2024-09-02 14:23:00 -0300

    feat: added db in test

--- aula_02/test/transaction.spec.ts
+++ aula_02/test/transaction.spec.ts
@@ -1,6 +1,7 @@
-import { test, beforeAll, afterAll, describe, expect } from 'vitest'
+import { test, beforeAll, afterAll, describe, expect, beforeEach } from 'vitest'
 import supertest from 'supertest'
 import { app } from '../src/app'
+import { execSync } from 'node:child_process'
 
 describe('Transaction routes', () => {
   beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transaction routes', () => {
 
   afterAll(async () => {
     await app.close()
+  })
+
+  beforeEach(async () => {
+    execSync('npm run knex migrate:rollback --all')
+    execSync('npm run knex migrate:latest')
   })
 
   test('User can create a new transaction', async () => {
@@ -44,7 +50,61 @@ describe('Transaction routes', () => {
         ...transactionValue,
       }),
     ])
+  })
 
-    console.log(listTransactionsResponse)
+  test('Should be able to get specific transaction', async () => {
+    const transactionValue = {
+      title: `teste ${Math.random() * 10}`,
+      amout: 1000,
+    }
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({ ...transactionValue, type: 'CREDIT_CARD' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    const listTransactionsResponse = await supertest(app.server)
+      .get('/transactions')
+      .set('Cookie', cookies as string[])
+      .expect(200)
+      .then((res) => res.body)
+    const id = listTransactionsResponse.at(0).id
+
+    const getTransactionsResponse = await supertest(app.server)
+      .get(`/transactions/${id}`)
+      .set('Cookie', cookies as string[])
+      .expect(200)
+      .then((res) => res.body)
+
+    expect(id).toEqual(getTransactionsResponse.id)
+  })
+
+  test('should be able to get the summary', async () => {
+    const transactionValue = {
+      title: `teste ${Math.random() * 10}`,
+      amout: 5000,
+    }
+    const createTransactionResponse = await supertest(app.server)
+      .post('/transactions')
+      .send({ ...transactionValue, type: 'CREDIT_CARD' })
+
+    const cookies = createTransactionResponse.get('Set-Cookie')
+
+    await supertest(app.server)
+      .post('/transactions')
+      .set('Cookie', cookies as string[])
+      .send({
+        title: `teste ${Math.random() * 10}`,
+        amout: 2000,
+        type: 'DEBIT',
+      })
+
+    const summaryTransactionsResponse = await supertest(app.server)
+      .get(`/transactions/summary`)
+      .set('Cookie', cookies as string[])
+      .expect(200)
+      .then((res) => res.body)
+
+    expect(summaryTransactionsResponse).toEqual({ amout: 3000 })
   })
 })
