cab47ceb4f31410a5152e75f3e7fc13c8e985eb3
Moisés Macedo <moises.castro@concentrix.com>
2024-09-02 16:31:38 -0300

    fix: changed server

--- aula_02/src/server.ts
+++ aula_02/src/server.ts
@@ -1,10 +1,11 @@
 import { app } from './app'
 import { env } from './env'
 
-const PORT = env.PORT || 3030
+const PORT = env.PORT
 app
   .listen({
     port: PORT,
+    host: '0.0.0.0',
   })
   .then(() => {
     console.log(`Http Server running in port:${PORT}`)
