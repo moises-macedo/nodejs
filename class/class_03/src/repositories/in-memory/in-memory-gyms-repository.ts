3127bc492ac7f0033b7116440f2e52db88a5edb2
Moisés Macedo <moises.castro@concentrix.com>
2024-11-04 17:59:22 -0300

    feat: added search gym name

--- class/class_03/src/repositories/in-memory/in-memory-gyms-repository.ts
+++ class/class_03/src/repositories/in-memory/in-memory-gyms-repository.ts
@@ -11,6 +11,12 @@ export class InMemoryGymsRepository implements GymsRepository {
     return gym
   }
 
+  async searchManyByQuery(query: string, page: number) {
+    return this.items
+      .filter((el) => el.title.includes(query))
+      .slice((page - 1) * 20, page * 20)
+  }
+
   async create(data: Prisma.GymCreateInput) {
     const gym = {
       id: data.id ?? randomUUID(),
